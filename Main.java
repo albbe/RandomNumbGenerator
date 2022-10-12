3f2888d1089281074c3e84a59c8341652dccaf7e
albbe <114727896+albbe@users.noreply.github.com>
2022-10-12 18:34:35 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args) {
+        // When declaring a constant, it is a convention to put the name in capital letters
+        final int n = 5;
+        ArrayList<Integer> generateNumbers = generateNumbers();
+        // Print the numbers
+	System.out.println("The following " + n + " numbers were generated:");
+        }
+
+   
+    }
